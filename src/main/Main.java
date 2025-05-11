a050933c78ebc97c91049635930c0ed30ce12ef7
iSmithmilla <jhonmilla1607@gmail.com>
2025-05-11 02:26:30 -0500

    fix- fix java class main and package main/resources  2025.05.11

---
+++ src/main/Main.java
@@ -0,0 +1,33 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String messageInitial = """
+                -------------------------------------
+                ¡Bienvenido/a al Conversor de Moneda!
+                -------------------------------------
+                
+                En este programa podrás realizar la conversión
+                de tipo de cambio de monedas como:
+                Dólar, Pesos Argentinos, Real Brasileño,
+                entre otros.""";
+
+        String messagePanel = """
+                ***************************************
+                  Seleccione una opción de conversión
+                ***************************************
+                1 .- Dólar =>> Peso argentino
+                2 .- Dólar =>> Real brasileño
+                3 .- Dólar =>> Peso colombiano
+                4 .- Dólar =>> Sol peruano
+                5 .- Peso argentino  =>> Dólar
+                6 .- Real brasileño  =>> Dólar
+                7 .- Peso colombiano =>> Dólar
+                8 .- Sol Peruano     =>> Dólar
+                """;
+
+
+
+    }
+}
